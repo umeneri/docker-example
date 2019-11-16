7b912bf4929f47d09af0c7316b371816f117eebf
umeneri <koishimst@gmail.com>
2019-11-16 22:00:45 +0900

    add sqs add main

---
+++ event-worker-queue/sqs/src/main/scala/SqsAddMain.scala
@@ -0,0 +1,14 @@
+import awscala._
+import awscala.sqs._
+
+object SqsAddMain extends App {
+  implicit val sqs: SQS = SQS.at(Region.Tokyo)
+
+  val queue = sqs.queue(" ls-test-staging-queue").map { queue =>
+    queue.add("message body")
+    queue.add("first", "second", "third")
+    println("messages added")
+  }
+
+  println("finish queuing")
+}
