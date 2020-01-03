caf5f4621fabaebcd4a18cc4e492cda1d9342f5c
umeneri <koishimst@gmail.com>
2020-01-03 14:48:34 +0900

    feat scripts

--- fargate-app/docker/build.sh
+++ fargate-app/docker/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+$(aws ecr get-login --no-include-email --region ap-northeast-1)
 docker build -t $1 $1
 docker tag $1:latest 772010606571.dkr.ecr.ap-northeast-1.amazonaws.com/staging-$1:latest
 docker push 772010606571.dkr.ecr.ap-northeast-1.amazonaws.com/staging-$1:latest
