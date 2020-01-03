20e361bc9829d97933fc97c37be03b1d16bae377
umeneri <koishimst@gmail.com>
2020-01-03 14:56:25 +0900

    fix script

--- fargate-app/docker/build.sh
+++ fargate-app/docker/build.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash
 
+function usage() {
+    cat <<EOF
+build docker image and upload to ecr.
+
+Usage:
+    $(basename ${0}) [frontend | backend]
+EOF
+
+return 0
+}
+
+if [[ $# -ne 1 ]] ; then
+    usage
+    exit 1
+else
+    target=$1
+fi
+
 $(aws ecr get-login --no-include-email --region ap-northeast-1)
-docker build -t $1 $1
-docker tag $1:latest 772010606571.dkr.ecr.ap-northeast-1.amazonaws.com/staging-$1:latest
-docker push 772010606571.dkr.ecr.ap-northeast-1.amazonaws.com/staging-$1:latest
+docker build -t $target $target
+docker tag $target:latest 772010606571.dkr.ecr.ap-northeast-1.amazonaws.com/staging-$target:latest
+docker push 772010606571.dkr.ecr.ap-northeast-1.amazonaws.com/staging-$target:latest
